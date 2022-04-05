d93595f7731b23de8f271581a459ced687977f76
Thomas Marble <tmarble3@gmail.com>
2022-04-05 13:37:08 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DapperInClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
