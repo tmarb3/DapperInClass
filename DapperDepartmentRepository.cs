94a821cb84bceb31368a98bc48e39a65af8a8ae4
Thomas Marble <tmarble3@gmail.com>
2022-04-09 11:59:27 -0600

    intial commit

---
+++ DapperDepartmentRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Dapper;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperInClass
+{
+    public class DapperDepartmentRepository : IDeparmentRepository
+    {
+        private readonly IDbConnection _connection;
+        public DapperDepartmentRepository(IDbConnection connection)
+        {
+            _connection = connection;
+        }
+        public IEnumerable<Department> GetAllDepartments()
+        {
+            var depos = _connection.Query<Department>("SELECT * FROM departments");
+
+            return depos;
+        }
+
+        public void InsertDepartment(string newDepartmentName)
+        {
+            _connection.Execute("INSERT INTO DEPARTMENTS (Name) VALUES (@departmentName);",
+            new { departmentName = newDepartmentName });
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+using System.IO;
 
 namespace DapperInClass
 {
@@ -6,7 +10,37 @@ namespace DapperInClass
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region Configuration
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string connString = config.GetConnectionString("DefaultConnection");
+            #endregion
+            IDbConnection conn = new MySqlConnection(connString);
+            
+            DapperDepartmentRepository repo = new DapperDepartmentRepository(conn);
+            Console.WriteLine("Hello, here are the current departments");
+            
+            var depos = repo.GetAllDepartments();
+
+            foreach (var depo in depos)
+            {
+                Console.WriteLine(depo.Name);
+            }
+
+            Console.WriteLine("Do you want to add a department?");
+            string userResponse = Console.ReadLine();
+
+            if (userResponse.ToLower() == "yes")
+            {   
+                Console.WriteLine("What is the name of the new department?");
+                var newDepartmentName = Console.ReadLine();
+                repo.InsertDepartment(newDepartmentName);
+                repo.GetAllDepartments();
+            }
+            Console.WriteLine("Have a great day.");
         }
     }
 }
